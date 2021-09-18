81b78718c116203024c2257ede49291525d91fd6
Jin1won <wlsdnjs16521@gmail.com>
2021-09-18 19:56:49 +0900

    아이템 삭제 기능 구현 및 디펜전시 인젝션을 통한 PageItemComponent 리팩토링

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { Component } from './components/component';
-import { Composable, PageComponent } from './components/page/page.js';
+import { Composable, PageComponent, PageItemComponent } from './components/page/page.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { NoteComponent } from './components/page/item/note.js';
@@ -8,7 +8,8 @@ import { TodoComponent } from './components/page/item/todo.js';
 class App{
     private readonly page:Component & Composable;
     constructor(appRoot: HTMLElement){
-        this.page = new PageComponent();
+        // 이제 다크모드 페이지나 다른 종류의 페이지 컴포넌트를 생성자에 전달만 해주면 다른 타입의 아이템을 만들 수 있다.
+        this.page = new PageComponent(PageItemComponent);
         this.page.attachTo(appRoot);
         
         const image = new ImageComponent('Image Title','https://picsum.photos/600/300');
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -3,7 +3,19 @@ import { BaseComponent,Component } from '../component.js';
 export interface Composable{
     addChild(child:Component):void;
 }
-class PageItemComponent extends BaseComponent<HTMLElement>{
+// 어떤 component든 꼭 cloase버튼이 필요하기 때문에 setoncomponent를 꼭 구현해야 하므로 interface로 규격해놓는다.
+interface SectionContainer extends Component, Composable{
+    setOnCloseListener(listener:OnCloseListener):void;
+}
+
+type OnCloseListener = () => void;
+
+// 여기서 생성자는  아무것도 받지 않지만, 생성자가 호출이 되면 sectioncontainer의 규격을 따라가는 어떤 클래스라도 타입에 맞는다.
+type SectionContainerConstructor = {
+    new():SectionContainer;
+}
+export class PageItemComponent extends BaseComponent<HTMLElement> implements SectionContainer{
+    private closeListener?:OnCloseListener;
     constructor(){
         super(`<li class="page-item">
                     <section class="page-item__body"></section>
@@ -11,20 +23,30 @@ class PageItemComponent extends BaseComponent<HTMLElement>{
                         <button class="close">&times;</button>
                     </div>
                 </li>`);
+        const cloasBtn = this.element.querySelector('.close')! as HTMLElement;
+        cloasBtn.onclick = () =>{
+            this.closeListener && this.closeListener();
+        } 
     }
     addChild(child:Component){
         const container = this.element.querySelector('.page-item__body')! as HTMLElement;
         child.attachTo(container);
     }
+    //외부에서 listener를 받아오는 함수
+    setOnCloseListener(listener:OnCloseListener){
+        this.closeListener = listener;
+    }
 }
 export class PageComponent extends BaseComponent<HTMLUListElement>{
-    //자식 클래스에선 무조건 부모 클래스의 생성자를 호출해야 한다.
-    constructor(){
+    constructor(private pageItemConstructor:SectionContainerConstructor){
         super('<ul class="page"></ul>');
     }
     addChild(section:Component){
-        const item = new PageItemComponent();
+        const item = new this.pageItemConstructor();
         item.addChild(section);
         item.attachTo(this.element, 'beforeend');
+        item.setOnCloseListener(()=>{
+            item.removeFrom(this.element);
+        })
     }
 }
